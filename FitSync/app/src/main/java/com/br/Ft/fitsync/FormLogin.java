111fc06ad7688a73cadd749d4645156d7e1086af
Vinicius Piovesan <viniciusmap22@gmail.com>
2024-10-22 20:35:22 -0300

    att front
    att front

--- FitSync/app/src/main/java/com/br/Ft/fitsync/FormLogin.java
+++ FitSync/app/src/main/java/com/br/Ft/fitsync/FormLogin.java
@@ -1,26 +1,33 @@
 package com.br.Ft.fitsync;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class FormLogin extends AppCompatActivity {
+
+    private TextView text_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_form_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        IniciarComponentes();
+
+        text_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FormLogin.this, Register.class);
+                startActivity(intent);
+            }
         });
+    }
 
-
+    private void IniciarComponentes() {
+        text_register = findViewById(R.id.text_register);
     }
 }
